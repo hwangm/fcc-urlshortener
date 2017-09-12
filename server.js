1f9ede56ae4e2affe60a00eba550362b0f2e2899
System <none>
2017-09-12 21:14:20 +0000

    Final version

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ var app = express();
 // but feel free to use whatever libs or frameworks you'd like through `package.json`.
 
 app.get('/', (req, res) => {
-  res.redirect('/new/http://www.google.com'); //for testing purposes
+  res.redirect('/new/hello'); //for testing purposes
   //res.send("If you would like to request a new shortened URL, use https://leaf-square.glitch.me/new/urlToShorten");
 });
 
@@ -27,13 +27,56 @@ app.get("/new/*", (req, res) => {
       });
     }
     else {
-      res.json(data);
+      data.short_url = 'https://' + req.hostname + '/' + data.short_url;
+      res.json({
+        'original_url': data.original_url,
+        'short_url': data.short_url
+      });
     }
   });
 });
 
+app.get('/*', (req, res) => {
+  var path = +req.path.split('/')[1];
+  if (isNaN(path)){
+    res.json({
+      'error': 'Invalid shortened URL - must be a number between 1 and 5000.'
+    });
+  }
+  else{
+    findOriginal(path, (err, data) => {
+      if (err) {
+        res.json({
+          'error': err
+        });
+      }
+      else {
+        res.redirect(data);
+      }
+    })
+  }
+});
+
+function findOriginal(url, callback){
+  mongo.connect(dburl, (err, db) => {
+    if (err) return callback(err);
+    var coll = db.collection('urls');
+    
+    coll.findOne({
+      short_url: url
+    }, (err, doc) => {
+      if (err) return callback(err);
+      if (doc == null){
+        return callback('No website found with this short URL.');
+      }
+      else{
+        return callback(null, doc.original_url);
+      }
+    });
+  });
+}
+
 function shorten(url, callback){
-  console.log('hello');
   if (validUrl.isHttpUri(url) || validUrl.isHttpsUri(url)){
     var data = {
       'original_url': url
@@ -56,14 +99,16 @@ function shorten(url, callback){
         else {
           createShortUrl(data, coll, (err, data) => {
             coll.insertOne(data, (err, r) => {
-              if (err) return callback(err);
-              console.log('inserted ' + r )
-              return callback(null, data);
-              db.close();
+              if (err) {
+                return callback(err);
+              }
+              else{
+                console.log('inserted ' + r )
+                return callback(null, data);
+                db.close();
+              }
             });
           });
-          
-
         }
       });
     });
@@ -72,19 +117,34 @@ function shorten(url, callback){
   
 }
 
-function createShortUrl(d, coll, callback){
+function createShortUrl(d, coll, callback){ 
   d.short_url = getRandomInt(1, 5000);
   var isUnique = false;
-  while(isUnique == false){
-    coll.findOne({
-      short_url: d.short_url
-    }, (err, doc) => {
-      if (err) return callback(err);
-      if (doc == {}) isUnique = true;
-      else d.short_url = getRandomInt(1, 5000);
-    });
-  }
-  return callback(null, d);
+  var count = 0;
+  console.log('isUnique starts as false');
+  console.log('value of short_url is ' + d.short_url);
+  findShortUrl(d.short_url, coll, (err, data) => {
+    if (err) return callback(err);
+    d.short_url = data;
+    return callback(null, d);
+  });
+}
+
+function findShortUrl(url, coll, callback){
+  coll.findOne({
+    short_url: url
+  }, (err, doc) => {
+    console.log('doc is ' + doc);
+    if (err) {
+      throw err;
+    }
+    if (doc == null) {
+      return callback(null, url);
+    }
+    else {
+      findShortUrl(getRandomInt(1, 5000), coll);
+    }
+  });
 }
                   
 
